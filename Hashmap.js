82a69e90dd5e43419464dd0b6a7dc7d2278869fb
Sc0rpX <mahfuzurrahman5609@gmail.com>
2026-06-02 07:36:13 +0600

    Implement initial Hashmap class with constructor and hash method

---
+++ Hashmap.js
@@ -0,0 +1,17 @@
+class Hashmap {
+    constructor(capacity = 16) {
+        this.capacity = capacity;
+        this.loadFactor = 0.75;
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+
+        return hashCode;
+    } 
+}
