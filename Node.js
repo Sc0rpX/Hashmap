99ea3f83b963803e8b415ce06c4682607bcaccee
Sc0rpX <mahfuzurrahman5609@gmail.com>
2026-06-02 08:15:12 +0600

    Refactor Node constructor to set nextNode to null by default

--- Node.js
+++ Node.js
@@ -1,7 +1,7 @@
 export default class Node {
-    constructor(key, value, nextNode = null) {
+    constructor(key, value) {
         this.key = key;
         this.value = value;
-        this.nextNode = nextNode;
+        this.nextNode = null;
     }
 }
